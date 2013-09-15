370645a838e67ea4150f349ff857d6867f685ae9
Sean Hood <me@seanhood.co.uk>
2013-09-15 21:18:18 +0100

    Improved loops, now randomises pixles

--- PiLiteClock.py
+++ PiLiteClock.py
@@ -1,48 +1,29 @@
 #!/usr/bin/python
 
-# PiLiteClock
-# Copyright (C) 2013 Sean Hood
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-# https://github.com/seanhood/PiLiteClock
-
 import math, datetime, sys
+from pprint import pprint
+import random
 
 lst = []
 
-h = 1
-while h < 13:
-	h = h + 1	
-	m = 1
-	while m < 5:
+hr = range(2,14)
+am = range(1,5)
+pm = range(6,10)
+
+for h in hr:
+	for m in am:
 		lst.append(str(h) + ',' + str(m))
-		m = m +1 
-h = 1
-while h < 13:
-	h = h + 1	
-	m = 6
-	while m < 10:
+
+for h in hr:
+	for m in pm:
 		lst.append(str(h) + ',' + str(m))
-		m = m +1	
 
 t = datetime.datetime.now()
 h = t.hour * 4
 m = math.ceil(float(t.minute) / 15)
-c = int(h + m)
+c = int(h + m - 1)
 
-def output(x):
+def output():
 	import serial
 	s = serial.Serial()
 	s.baudrate = 9600
@@ -56,11 +37,15 @@ def output(x):
 		sys.exit(1)
 		
 	s.write("$$$ALL,OFF\r")
+
+	pix = range(c)
+	random.shuffle(pix)
 	
-	for i in range(c):
-		s.write("$$$P" + lst[i] + ",ON\r")	
+	for p in pix:
+		s.write("$$$P" + lst[p] + ",ON\r")
+
 
 ## Lets Run
-output(c)
+output()
 
 sys.exit()
